aa8b516ee0b4b34ea88caf93f854ed4cc6bd577b
budha.jethava <budha.jethava@spec-india.com>
2026-08-12 17:19:57 +0530

    Added terraform and deployment stuff

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,9 +1,11 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   count = 2
   cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
   map_public_ip_on_launch = true
-
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
     Name = "${var.tag_name}-public-subnet-${count.index}"
     Environment = var.env
@@ -15,7 +17,7 @@ resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   count = 2
   cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index+10)
-
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
     Name = "${var.tag_name}-private-subnet-${count.index}"
     Environment = var.env
